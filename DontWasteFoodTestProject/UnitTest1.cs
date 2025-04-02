96661528c09ca7c089970cc61d3368b6a8a88122
Luke Spires <luke.spires@tba.group>
2025-04-02 09:14:46 +0100

    first checkin

---
+++ DontWasteFoodTestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DontWasteFoodTestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
